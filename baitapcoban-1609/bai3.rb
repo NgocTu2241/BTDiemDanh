61e2974166333c2661f7fa6095bf7e94a50d6910
Nguyen Tu <80658387+NgocTu2241@users.noreply.github.com>
2022-09-17 13:19:43 +0700

    Add files via upload

---
+++ baitapcoban-1609/bai3.rb
@@ -0,0 +1,8 @@
+#Viết chương trình để nhắc người dùng nhập tên của họ, sau đó in kết quả trên màn hình.
+while name = " "
+  puts 'Vui long nhap ten'
+  puts 'Ten cua ban la '
+  name = gets
+  break
+end
+puts "Ten da nhap la #{name}"
